f4ceda2d9902b2dfb29dd0f00d8e4be39963f23c
grantonokello <kubasugrantonokello@gmail.com>
2023-01-06 22:38:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, flash
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "gvsyuagfjhcsgzhjgvcjhsgjdzfd"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+db = SQLAlchemy(app)
+class Store(db.Model):
+     item = db.Column(db.Integer, primary_key=True)
+     number = db.Column(db.Integer)
+     buyingPrice = db.Column(db.Float)
+
+class Sold(db.Model):
+    item = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+    sellingPrice = db.Column(db.Float)
+    date = db.Column(db.DateTime)
+
+
+
+class SELL:
+    def seller(self, item, number, sellingPrice):
+        self.item = item
+        self.number = number
+        self.sellingPrice = sellingPrice
+        store = Store()
+        date = datetime.today()
+        query = store(item=item, number=number, sellingPrice=sellingPrice, date=date.strftime("d%-m%-y%"))
+        return "Item sold!!"
+
+    def manager(self):
+        data = Store.query.all()
+        return data
+
+    def storage(self, item, number, buyingPrice):
+        self.item = item
+        self.number = number
+        self.buyingPrice = buyingPrice
+        store = Store()
+        query = store(item=item, number=number, BuyingPrice=buyingPrice)
+
+@app.route("/layout")
+def layout():
+    return render_template("layout.html")
+
+@app.route("/")
+def selling():
+    return render_template("selling.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email2"]
+        password = request.form["password2"]
+    return render_template("login.html")
+
+@app.route("/view")
+def view():
+    return "hello"
+
+
+@app.route("/stock")
+def stoke():
+    return "hello"
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
